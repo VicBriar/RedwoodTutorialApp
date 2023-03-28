5df41c71a94b37d1ae51c1f9b5d10ca0ad75c1fe
Victoria McCrady <spartanginger@gmail.com>
2023-03-28 14:41:08 -0700

    feat(web/src/components and routes): article cell displays single article
    tutorial, https://redwoodjs.com/docs/tutorial/chapter2/routing-params "displaying a blog post"
    section is next. Right now, article dumps a query using the id passed from articles, to articleCell,
    which articlePage displays

--- web/src/pages/ArticlePage/ArticlePage.tsx
+++ web/src/pages/ArticlePage/ArticlePage.tsx
@@ -1,15 +1,16 @@
 
 import { MetaTags } from '@redwoodjs/web'
+import ArticleCell from 'src/components/ArticleCell'
 
-const ArticlePage = () => {
+interface Props {
+  id: number
+}
+
+const ArticlePage = ({id}: Props) => {
   return (
     <>
       <MetaTags title="Article" description="Article page" />
-
-      <h1>ArticlePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ArticlePage/ArticlePage.tsx</code>
-      </p>
+      <ArticleCell id={id} />
     </>
   )
 }
